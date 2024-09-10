3c0ce5ea193971f2bf80ad36a50edf599a039be3
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2024-09-10 20:49:07 +0530

    getweather

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -3,7 +3,7 @@ import { Search, MapPin, Wind } from "react-feather";
 import dateFormat from "dateformat";
 
 function Weather() {
-  const [city, setCity] = useState();
+  const [city, setCity] = useState("Gwalior");
   const [weather, setWeather] = useState({});
 
   const apiKey = "ecfd88dcf1e4639dcf559a48f2f37f88";
@@ -16,20 +16,22 @@ function Weather() {
         return json;
       });
   };
+
   const getWeatherbyCity = async () => {
     const weatherData = await getWeather(city);
     setWeather(weatherData);
-    setCity("");
+    // No need to reset the city here
   };
 
   const renderDate = () => {
     let now = new Date();
-    return dateFormat(now, "dddd,mmmm dS,h:MM TT");
+    return dateFormat(now, "dddd, mmmm dS, h:MM TT");
   };
+
   return (
     <>
       <div className="app">
-        <h1>Weater App</h1>
+        <h1>Weather App</h1>
         <div className="input-wrapper">
           <input
             type="text"
@@ -37,50 +39,47 @@ function Weather() {
             onChange={(e) => setCity(e.target.value)}
             placeholder="Please Enter City Name"
           />
-          <button onClick={() => getWeatherbyCity()}>
-            <Search></Search>
+          <button onClick={getWeatherbyCity}>
+            <Search />
           </button>
         </div>
-        {weather && weather.weather && (
+        {weather && weather.weather ? (
           <div className="content">
             <div className="location d-flex">
-              <MapPin></MapPin>
+              <MapPin />
               <h2>
                 {weather.name} <span>({weather.sys.country})</span>
               </h2>
             </div>
-            <p className="datetext"> {renderDate}</p>
+            <p className="datetext"> {renderDate()}</p>
             <div className="weatherdesc d-flex flex-c">
               <img
-                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}10d@2x.png`}
-                alt=""
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt="weather icon"
               />
               <h3>{weather.weather[0].description}</h3>
             </div>
             <div className="tempstats d-flex flex-c">
               <h1>
-                {weather.main.temp} <span>&deg:C</span>
+                {weather.main.temp} <span>&deg;C</span>
               </h1>
               <h3>
                 Feels like {weather.main.feels_like}
-                <span>&deg:C</span>
+                <span>&deg;C</span>
               </h3>
             </div>
             <div className="windstats d-flex">
-              <Wind></Wind>
+              <Wind />
               <h3>
-                {" "}
-                Wind is {weather.wind.speed} Knots in {weather.wind.deg}&deg:
+                Wind is {weather.wind.speed} Knots in {weather.wind.deg}&deg;
               </h3>
             </div>
           </div>
-        )}
-        {!weather.weather && (
+        ) : (
           <div className="content">
-            <h4> No Data Found!</h4>
+            <h4>No Data Found!</h4>
           </div>
         )}
-        {/* <p>{JSON.stringify(weather)}</p> */}
       </div>
     </>
   );
